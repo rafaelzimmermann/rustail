52f57fbfb7f748ba6dbe2a14e1c6f951dfbfc960
Rafael Zimmermann <git@chess.mozmail.com>
2023-03-06 15:38:03 +0100

    Extract argument parsing to a module

---
+++ src/argparse.rs
@@ -0,0 +1,18 @@
+pub mod parser {
+    use std::env;
+    use std::ffi::OsStr;
+    use std::path::Path;
+
+    pub fn files() -> Vec<String> {
+        return env::args().skip(1).collect();
+    }
+
+    pub fn app_name() -> Option<String> {
+        return env::args().next()
+            .as_ref()
+            .map(Path::new)
+            .and_then(Path::file_name)
+            .and_then(OsStr::to_str)
+            .map(String::from)
+    }
+}
